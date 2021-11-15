ec05adbc397434efb8dc0a2d9f58299b6a647445
Ning <9544475+ningjianshan@user.noreply.gitee.com>
2021-11-15 16:00:39 +0800

    init gshop

---
+++ src/components/FooterGuide/FooterGuide.vue
@@ -0,0 +1,165 @@
+<template>
+  <div class="footer_guide border-1px">
+    <router-link
+      href="javascript:;"
+      class="guide_item"
+      :to="{ path: '/msite' }"
+      active-class="on"
+    >
+      <span class="item_icon">
+        <i class="iconfont icon-waimai"></i>
+      </span>
+      <span>外卖</span>
+    </router-link>
+    <router-link
+      href="javascript:;"
+      class="guide_item"
+      :to="{ path: '/search' }"
+      active-class="on"
+    >
+      <span class="item_icon">
+        <i class="iconfont icon-search">&#xe634;</i>
+      </span>
+      <span>搜索</span>
+    </router-link>
+    <router-link
+      href="javascript:;"
+      class="guide_item"
+      :to="{ path: '/order' }"
+      active-class="on"
+    >
+      <span class="item_icon">
+        <i class="iconfont icon-dingdan">&#xe619;</i>
+      </span>
+      <span>订单</span>
+    </router-link>
+    <router-link
+      href="javascript:;"
+      class="guide_item"
+      :to="{ path: '/profile' }"
+      active-class="on"
+    >
+      <span class="item_icon">
+        <i class="iconfont icon-geren">&#xe608;</i>
+      </span>
+      <span>我的</span>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="stylus" scoped>
+$green = #02a774;
+$yellow = #F5A100;
+$bc = #e4e4e4;
+
+// 一像素下边框
+bottom-border-1px($color) {
+  position: relative;
+  border: none;
+
+  &:after {
+    content: '';
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    height: 1px;
+    background-color: $color;
+    transform: scaleY(0.5);
+  }
+}
+
+// 一像素上边框
+top-border-1px($color) {
+  position: relative;
+
+  &::before {
+    content: '';
+    position: absolute;
+    z-index: 200;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 1px;
+    background-color: $color;
+  }
+}
+
+// 根据像素比缩放1px像素边框
+@media only screen and (-webkit-device-pixel-ratio: 2) {
+  .border-1px {
+    &::before {
+      transform: scaleY(0.5);
+    }
+  }
+}
+
+@media only screen and (-webkit-device-pixel-ratio: 3) {
+  .border-1px {
+    &::before {
+      transform: scaleY(0.333333);
+    }
+  }
+}
+
+// 根据像素比来使用 2x图 3x图
+bg-image($url) {
+  background-image: url($url + '@2x.png');
+
+  @media (-webkit-min-device-pixel-ratio: 3), (min-device-pixel-ratio: 3) {
+    background-image: url($url + '@3x.png');
+  }
+}
+
+// 清除浮动
+clearFix() {
+  *zoom: 1;
+
+  &::after {
+    content: '';
+    display: block;
+    clear: both;
+  }
+}
+
+.footer_guide {
+  top-border-1px(#e4e4e4);
+  position: fixed;
+  z-index: 100;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #fff;
+  width: 100%;
+  height: 50px;
+  display: flex;
+
+  .guide_item {
+    display: flex;
+    flex: 1;
+    text-align: center;
+    flex-direction: column;
+    align-items: center;
+    margin: 5px;
+    color: #999999;
+
+    &.on {
+      color: #02a774;
+    }
+
+    span {
+      font-size: 12px;
+      margin-top: 2px;
+      margin-bottom: 2px;
+
+      .iconfont {
+        font-size: 22px;
+      }
+    }
+  }
+}
+</style>
---
+++ src/pages/Search/Search.vue
@@ -0,0 +1,132 @@
+<template>
+  <section class="search">
+    <HeaderTop title="搜索"></HeaderTop>
+    <form class="search_form" action="#">
+      <input
+        type="search"
+        name="search"
+        placeholder="请输入商家或美食名称"
+        class="search_input"
+      />
+      <input type="submit" name="submit" class="search_submit" />
+    </form>
+  </section>
+</template>
+
+<script>
+import HeaderTop from "../../components/HeaderTop/HeaderTop.vue";
+export default {
+  components: {
+    HeaderTop,
+  },
+};
+</script>
+
+<style lang="stylus" scoped>
+// 一像素下边框
+bottom-border-1px($color) {
+  position: relative;
+
+  &::before {
+    content: '';
+    position: absolute;
+    z-index: 200;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    height: 1px;
+    background-color: $color;
+    transform: scaleY(0.5);
+  }
+}
+
+// 一像素上边框
+top-border-1px($color) {
+  position: relative;
+
+  &::before {
+    content: '';
+    position: absolute;
+    z-index: 200;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 1px;
+    background-color: $color;
+  }
+}
+
+// 根据像素比缩放1px像素边框
+@media only screen and (-webkit-device-pixel-ratio: 2) {
+  .border-1px {
+    &::before {
+      transform: scaleY(0.5);
+    }
+  }
+}
+
+@media only screen and (-webkit-device-pixel-ratio: 3) {
+  .border-1px {
+    &::before {
+      transform: scaleY(0.333333);
+    }
+  }
+}
+
+// 根据像素比来使用 2x图 3x图
+bg-image($url) {
+  background-image: url($url + '@2x.png');
+
+  @media (-webkit-min-device-pixel-ratio: 3), (min-device-pixel-ratio: 3) {
+    background-image: url($url + '@3x.png');
+  }
+}
+
+// 清除浮动
+clearFix() {
+  *zoom: 1;
+
+  &::after {
+    content: '';
+    display: block;
+    clear: both;
+  }
+}
+
+&.search {
+  width: 100%;
+
+  .search_form {
+    clearFix();
+    margin-top: 45px;
+    background-color: #fff;
+    padding: 12px 8px;
+
+    input {
+      height: 35px;
+      padding: 0 4px;
+      border-radius: 2px;
+      font-weight: bold;
+      outline: none;
+
+      &.search_input {
+        float: left;
+        width: 79%;
+        border: 4px solid #f2f2f2;
+        font-size: 14px;
+        color: #333;
+        background-color: #f2f2f2;
+      }
+
+      &.search_submit {
+        float: right;
+        width: 18%;
+        border: 4px solid #02a774;
+        font-size: 16px;
+        color: #fff;
+        background-color: #02a774;
+      }
+    }
+  }
+}
+</style>
